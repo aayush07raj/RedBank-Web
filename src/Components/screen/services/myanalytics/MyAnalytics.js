825b336253099175bf0fc286f8159981fad95b8e
aayush07raj <aayush.raj@nineleaps.com>
2021-03-01 13:19:41 +0530

    analytics API Integration done

--- src/Components/screen/services/myanalytics/MyAnalytics.js
+++ src/Components/screen/services/myanalytics/MyAnalytics.js
@@ -4,6 +4,11 @@ import {
   Grid,
   makeStyles,
   Paper,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  Select,
+  InputLabel,
   Typography,
   Divider,
 } from "@material-ui/core";
@@ -23,37 +28,64 @@ const useStyles = makeStyles((theme) => ({
   charts: {
     margin: theme.spacing(3),
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
 }));
 
 function MyAnalytics() {
   const classes = useStyles();
   const loggedInState = useSelector((state) => state.loggedIn);
+  const [currYear, setCurrYear] = useState(()=>{return new Date().getFullYear()})
+  const [yearlySales , setYearlySales] = useState([])
 
-  useEffect(() => {
+  const [currMonth, setCurrMonth] = useState("")
+  const [monthlySales , setMonthlySales] = useState([])
+  
+
+  const handleYearChange = (e)=>{
+    setCurrYear(e.target.value)  
+
     axios
-    .get(`http://localhost:8080/salesanalytics/fetchall/${2020}`,{
+    .get(`http://localhost:8080/salesanalytics/fetchall/${e.target.value}`,{
       headers:{
         Authorization: "Bearer " + loggedInState.userToken,
       },
     })
     .then((response)=>{
-      console.log(response.data)
-      forMonths();
+      setYearlySales(response.data)
+      console.log(response)
     })
-    return () => {
-     
-    }
-  },[] );
+  }
 
-  const forMonths = () =>{
+  useEffect(()=>{
     axios
-    .get(`http://localhost:8080/salesanalytics/fetchcurrentmonth/${1}`,{
+    .get(`http://localhost:8080/salesanalytics/fetchall/${currYear}`,{
       headers:{
         Authorization: "Bearer " + loggedInState.userToken,
       },
     })
     .then((response)=>{
-      console.log(response.data)
+      setYearlySales(response.data)
+      console.log(response)
+    })
+  },[])
+
+
+  const handleMonthChange = (e)=>{
+    setCurrMonth(e.target.value)
+
+    axios
+    .get(`http://localhost:8080/salesanalytics/fetchcurrentmonth/${e.target.value}`,{
+      headers:{
+        Authorization: "Bearer " + loggedInState.userToken,
+      },
+    })
+    .then((response)=>{
+      console.log(response)
+      setMonthlySales(response.data);
+
     })
   }
 
@@ -66,42 +98,55 @@ function MyAnalytics() {
         <Typography variant="h6">
           Here you can view statistics about your data that is present with us
         </Typography>
+        
       </Paper>
       <Container maxWidth="lg">
         <Grid container justify="center" className={classes.charts} spacing={5}>
           <Grid item xs={12}>
-            <Typography variant="h5">Sales and Revenue stats:</Typography>
+            <Typography variant="h5">Sales Yearly wise Sales:</Typography>
+                <FormControl className={classes.formControl}>
+                <InputLabel >Select Year</InputLabel>
+                  <Select
+                    value={currYear}
+                    onChange={handleYearChange}
+                    renderValue={(currYear) => `${currYear}`}>
+                    <MenuItem value={"2020"}>2021</MenuItem>
+                    <MenuItem value={"2020"}>2020</MenuItem>
+                    <MenuItem value={"2019"}>2019</MenuItem>
+                    <MenuItem value={"2018"}>2018</MenuItem>
+                    <MenuItem value={"2017"}>2017</MenuItem>
+                    <MenuItem value={"2016"}>2016</MenuItem>
+                    <MenuItem value={"2015"}>2015</MenuItem>
+                  </Select>
+                  <FormHelperText>Analytics for: {currYear}</FormHelperText>
+                </FormControl>
+                <Grid item  xs={12} align="center">
+                  <BarChart data={yearlySales.dataset} legends={yearlySales.legend} labels={yearlySales.labels} />
+                </Grid>
+
+                <Typography variant="h5">Monthly wise Sales:</Typography>
+                <FormControl className={classes.formControl}>
+                <InputLabel >Select Month</InputLabel>
+                  <Select
+                    value={currMonth}
+                    onChange={handleMonthChange}
+                    >
+                    <MenuItem value={"January"}>January</MenuItem>
+                    <MenuItem value={"Febuary"}>Febuary</MenuItem>
+                    <MenuItem value={"March"}>March</MenuItem>
+                    <MenuItem value={"April"}>April</MenuItem>
+                    <MenuItem value={"May"}>May</MenuItem>
+                    <MenuItem value={"June"}>June</MenuItem>
+                    <MenuItem value={"July"}>July</MenuItem>
+                  </Select>
+                  <FormHelperText>Analytics for: {currMonth},{currYear}</FormHelperText>
+                </FormControl>
+                <Grid item  xs={12} align="center">
+                  <BarChart data={monthlySales.data} legends={monthlySales.lagend} labels={monthlySales.labels} />
+                </Grid>
           </Grid>
 
-          <Grid item xs={12} sm={6}>
-            <BarChart title="MAY" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <BarChart title="JUNE" />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <BarChart title="JULY" />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <BarChart title="AUGUST" />
-          </Grid>
-        </Grid>
-
-        <Grid container justify="center" className={classes.charts} spacing={5}>
-          <Grid item xs={12}>
-            <Typography variant="h5">Inventory stats :</Typography>
-          </Grid>
-
-          <Grid item xs={12} sm={4}>
-            <LineChart title="BLOOD" />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <LineChart title="PLASMA" />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <LineChart title="PLATELETS" />
-          </Grid>
+          
         </Grid>
       </Container>
       <Footer />
