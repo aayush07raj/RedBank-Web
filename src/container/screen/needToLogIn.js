dff713f248bba67c5054630bd6289a7672adfed9
aayush07raj <aayush.raj@nineleaps.com>
2021-03-21 14:58:40 +0530

    working on url,needtoLogin made

--- src/container/screen/needToLogIn.js
+++ src/container/screen/needToLogIn.js
@@ -1,11 +1,38 @@
 import React from "react";
+import LoggedOutNavbar from "../../component/loggedoutNavbar";
+import notFound from "../../assets/images/notFound.svg";
+import { Route, Redirect } from "react-router-dom";
+
+import { Link, useHistory } from "react-router-dom";
+import {
+    Grid,
+    Paper,
+    TextField,
+    Button,
+    Backdrop,
+    Typography,
+    CircularProgress,
+  } from "@material-ui/core";
+
 
 function NotFound() {
   return (
     <>
-      <h1>Error 404 : Page not found </h1>
-      <h1>You need to log in first to view this page</h1>
+    <LoggedOutNavbar/>
+    <Grid>
+    <Grid item xs={12} style={{marginTop:"20px"}} container justify="center" alignItems="center">
+          <img src={notFound} alt="verify" style={{ width: "40%" }} />
+    </Grid>
+    <Grid item xs={12} container justify="center" alignItems="center">
+        <h1>Error 404 : Page not found. You need to log in first to view this page </h1>
+    </Grid>  
+    <Grid item xs={12} container justify="center" alignItems="center">
+       <Button style={{marginTop:"20p"}} variant="contained" component={Link} to="/Login"> Login</Button>
+    </Grid>  
+    </Grid>
+    
       
+
     </>
   );
 }
