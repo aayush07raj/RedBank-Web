8caab173029337174d70c0227108221433884612
aayush07raj <aayush.raj@nineleaps.com>
2021-03-23 21:44:15 +0530

    piechart and barchart changes

--- src/container/ServicesStack/Analytics/barChart.js
+++ src/container/ServicesStack/Analytics/barChart.js
@@ -2,63 +2,41 @@ import React from "react";
 import { Bar } from "react-chartjs-2";
 
 function BarChart({ labels, legends, data, type }) {
-  // cause of error ->
-  // in case of monthly sales,revenue,purchases data is array
-  // in case of yearly sales,revenue,purchases data is an object
-
-  // in case of yearly.... labels is not present and data is an object
-
   return (
     <>
-      {type === "monthly" ? (
-        <Bar
-          data={{
-            labels: labels,
-            datasets: [
-              {
-                label: legends,
-                data: data,
-                backgroundColor: "#E94364",
-                borderColor: "#E94364",
-                borderWidth: 1,
-              },
-            ],
-          }}
-          height={220}
-          width={600}
-        />
-      ) : (
-        <Bar
-          data={{
-            labels: labels,
-            datasets: [
-              {
-                label: "blood",
-                data: data.bloodObject,
-                backgroundColor: "blue",
-                borderColor: "blue",
-                borderWidth: 1,
-              },
-              {
-                label: "plasma",
-                data: data.plasmaObject,
-                backgroundColor: "red",
-                borderColor: "red",
-                borderWidth: 1,
-              },
-              {
-                label: "platelets",
-                data: data.plateletObject,
-                backgroundColor: "grey",
-                borderColor: "grey",
-                borderWidth: 1,
-              },
-            ],
-          }}
-          height={220}
-          width={600}
-        />
-      )}
+      <Bar
+        data={{
+          labels: labels,
+          datasets: [
+            {
+              label: "blood",
+              data: data.bloodObject,
+              backgroundColor: "rgba(233, 67, 100, 0.2)",
+              borderColor: "rgba(233, 67, 100, 1)",
+              hoverBackgroundColor: "rgba(233, 67, 100, 0.5)",
+              borderWidth: 1,
+            },
+            {
+              label: "plasma",
+              data: data.plasmaObject,
+              backgroundColor: "rgba(51, 153, 255, 0.2)",
+              borderColor: "rgba(51, 153, 255, 1)",
+              hoverBackgroundColor: "rgba(51, 153, 255, 0.5)",
+              borderWidth: 1,
+            },
+            {
+              label: "platelets",
+              data: data.plateletObject,
+              backgroundColor: "rgba(155, 231, 91, 0.2)",
+              borderColor: "rgba(155, 231, 91, 1)",
+              hoverBackgroundColor: "rgba(155, 231, 91, 0.5)",
+              borderWidth: 1,
+            },
+          ],
+        }}
+        height={220}
+        width={600}
+      />
     </>
   );
 }
